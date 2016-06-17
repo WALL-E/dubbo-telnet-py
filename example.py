165ce115bf464ab763a2bed97f8c260ff2400a7a
zhangzheng <18601254750.163.com>
2016-06-17 11:12:07 +0800

    完成setup.py

---
+++ example.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import dubbo_telnet
+
+if __name__ == '__main__':
+    Host = '192.168.1.203'  # Doubble服务器IP
+    Port = 28008  # Doubble服务端口
+
+    # 初始化dubbo对象
+    conn = dubbo_telnet.dubbo(Host, Port)
+
+    # 设置telnet连接超时时间
+    conn.set_connect_timeout(10)
+
+    # 设置dubbo服务返回响应的编码
+    conn.set_encoding('gbk')
+
+    interface = 'com.zrj.pay.trade.api.QueryTradeService'
+    method = 'tradeDetailQuery'
+    param = '{"id": "nimeide"}'
+    print conn.invoke(interface, method, param)
+
+    command = 'invoke com.zrj.pay.trade.api.QueryTradeService.tradeDetailQuery({"id":"nimeide"})'
+    print conn.do(command)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dubbo_telnet',
+      version='0.1',
+      description='Dubbo Telnet Client of python',
+      url='https://github.com/WALL-E/dubbo-telnet-py',
+      author='zhangzheng',
+      author_email='zhangzheng@qianbao.com',
+      license='MIT',
+      packages=['dubbo_telnet'],
+      zip_safe=False)
