7b2ff07c4a904cac4841db5df558f16bbe25b462
zhangzheng <18601254750.163.com>
2016-06-17 15:40:59 +0800

    更新PYPI信息

--- setup.py
+++ setup.py
@@ -39,8 +39,8 @@ setup(name='dubbo_telnet',
         "Operating System :: MacOS",
         "Operating System :: POSIX",
         "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 2.6"
-        "Programming Language :: Python :: 2.7"
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Communications",
