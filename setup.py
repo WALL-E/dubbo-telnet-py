5e0a78a656a02a892ec29775d6a0a2724c97dbc1
zhangzheng <18601254750.163.com>
2016-06-17 12:01:49 +0800

    更新setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
+import os
 from setuptools import setup
 
+THISDIR = os.path.dirname(os.path.abspath(__file__))
+os.chdir(THISDIR)
+
+VERSION = open("version.txt").readline().strip()
+HOMEPAGE = 'https://github.com/WALL-E/dubbo-telnet-py'
+DOWNLOAD_BASEURL = "https://github.com/WALL-E/dubbo-telnet-py/raw/master/dist/"
+DOWNLOAD_URL = DOWNLOAD_BASEURL + "dubbo_telnet-%s-py2.6.egg" % VERSION
+
 setup(name='dubbo_telnet',
-      version='1.0.0b2',
+      version=VERSION,
       description='Dubbo Telnet Client of python',
-      url='https://github.com/WALL-E/dubbo-telnet-py',
+      long_description=open("README.md").read(),
+      url=HOMEPAGE,
+      download_url=DOWNLOAD_URL,
       author='zhangzheng',
       author_email='zhangzheng@qianbao.com',
       license='MIT',
