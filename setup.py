e5458688dd4f01561eed5f64d5307fc09d4bff36
zhangzheng <18601254750.163.com>
2016-06-17 15:39:21 +0800

    setup.py添加分类信息

--- setup.py
+++ setup.py
@@ -19,16 +19,33 @@ except AttributeError:
 DOWNLOAD_URL = DOWNLOAD_BASEURL + "dubbo_telnet-%s-py%s.%s.egg" % (VERSION, major, minor)
 
 setup(name='dubbo_telnet',
-      version=VERSION,
-      description='Dubbo Telnet Client of python',
-      long_description=open("README.PYPI").read(),
-      keywords=(
-          "dubbo, dubbo_telnet, telnet,"
-      ),
-      url=HOMEPAGE,
-      download_url=DOWNLOAD_URL,
-      author='zhangzheng',
-      author_email='zhangzheng@qianbao.com',
-      license='unlicense',
-      packages=['dubbo_telnet'],
-      zip_safe=False)
+    version=VERSION,
+    description='Dubbo Telnet Client of python',
+    long_description=open("README.PYPI").read(),
+    keywords=(
+        "dubbo, dubbo_telnet, telnet,"
+    ),
+    url=HOMEPAGE,
+    download_url=DOWNLOAD_URL,
+    author='zhangzheng',
+    author_email='zhangzheng@qianbao.com',
+    license='unlicense',
+    packages=['dubbo_telnet'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2.6"
+        "Programming Language :: Python :: 2.7"
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Communications",
+        "Topic :: System :: Networking",
+        "Topic :: Software Development :: Libraries :: Java Libraries",
+    ],
+    zip_safe=False
+)
